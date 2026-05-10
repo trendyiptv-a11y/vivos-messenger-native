e1f01caf6e8356326aa1bede0283fa742205be8d
Sergiu B. <trendyiptv@gmail.com>
2026-05-10 19:00:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import "event-target-shim"
 import { registerVivosCallV2NotifeeBackgroundHandler } from "./lib/calls-v2/notifeeCallV2"
+import { registerVivosCallV2FcmBackgroundHandler } from "./lib/calls-v2/fcmCallHandler"
 
 registerVivosCallV2NotifeeBackgroundHandler()
+registerVivosCallV2FcmBackgroundHandler()
 
 import "expo-router/entry"
