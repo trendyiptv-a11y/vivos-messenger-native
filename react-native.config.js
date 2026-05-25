b40ec1266a14ee21a0b683bd35ddecca2d14cf75
Sergiu B. <trendyiptv@gmail.com>
2026-05-25 20:42:53 +0200

    Limit Firebase autolinking override to iOS only

--- react-native.config.js
+++ react-native.config.js
@@ -1,14 +1,20 @@
-module.exports = {
-  dependencies: {
-    "@react-native-firebase/app": {
-      platforms: {
-        ios: null,
+const isIosBuild = process.env.EAS_BUILD_PLATFORM === "ios" || process.env.EXPO_OS === "ios"
+
+if (!isIosBuild) {
+  module.exports = {}
+} else {
+  module.exports = {
+    dependencies: {
+      "@react-native-firebase/app": {
+        platforms: {
+          ios: null,
+        },
+      },
+      "@react-native-firebase/messaging": {
+        platforms: {
+          ios: null,
+        },
       },
     },
-    "@react-native-firebase/messaging": {
-      platforms: {
-        ios: null,
-      },
-    },
-  },
+  }
 }
