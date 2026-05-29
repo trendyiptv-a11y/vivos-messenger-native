6730ccd44f29f015e1b59ba272d58a9bf81dcbff
Sergiu B. <trendyiptv@gmail.com>
2026-05-29 14:32:05 +0200

    Register VIVOS call foreground service on app startup

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 import "event-target-shim"
-import { registerVivosCallV2NotifeeBackgroundHandler } from "./lib/calls-v2/notifeeCallV2"
+import {
+  registerVivosCallV2ForegroundService,
+  registerVivosCallV2NotifeeBackgroundHandler,
+} from "./lib/calls-v2/notifeeCallV2"
 import { registerVivosCallV2FcmBackgroundHandler } from "./lib/calls-v2/fcmCallHandler"
 
+registerVivosCallV2ForegroundService()
 registerVivosCallV2NotifeeBackgroundHandler()
 registerVivosCallV2FcmBackgroundHandler()
 
