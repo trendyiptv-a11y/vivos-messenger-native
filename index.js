db4c5e18ce8e3ae8db532f989c3e516a17f9b3f5
Sergiu B. <trendyiptv@gmail.com>
2026-05-29 17:28:50 +0200

    Rollback foreground service startup registration

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import "event-target-shim"
-import {
-  registerVivosCallV2ForegroundService,
-  registerVivosCallV2NotifeeBackgroundHandler,
-} from "./lib/calls-v2/notifeeCallV2"
+import { registerVivosCallV2NotifeeBackgroundHandler } from "./lib/calls-v2/notifeeCallV2"
 import { registerVivosCallV2FcmBackgroundHandler } from "./lib/calls-v2/fcmCallHandler"
 
-registerVivosCallV2ForegroundService()
 registerVivosCallV2NotifeeBackgroundHandler()
 registerVivosCallV2FcmBackgroundHandler()
 
