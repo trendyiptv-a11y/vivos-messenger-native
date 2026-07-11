4a4f550097ddb20e8ee13d324a9f6295be0e9d6f
Sergiu B. <trendyiptv@gmail.com>
2026-07-11 17:22:40 +0200

    Register active call foreground service early

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import "event-target-shim"
 import { registerVivosCallV2NotifeeBackgroundHandler } from "./lib/calls-v2/notifeeCallV2"
 import { registerVivosCallV2FcmBackgroundHandler } from "./lib/calls-v2/fcmCallHandler"
+import { registerVivosCallForegroundService } from "./lib/calls-v2/callForegroundService"
 
+registerVivosCallForegroundService()
 registerVivosCallV2NotifeeBackgroundHandler()
 registerVivosCallV2FcmBackgroundHandler()
 
